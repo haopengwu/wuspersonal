02d4b6b125a703881ab5b3f536e99688da1790a9
haopengwu <1749256482@qq.com>
2020-04-07 23:03:54 +0800

    Add files via upload

---
+++ fileReader.cpp
@@ -0,0 +1,55 @@
+
+
+    #include<string>
+
+     #include<iostream>
+     #include <fstream>
+
+int main(){
+	//Set file name and path
+	std::string filename = "betty.raw";
+
+	//Open binary file for reading
+	std::ifstream myfile(filename.c_str(), std::ios::in | std::ios::binary);
+
+
+	if (myfile.is_open()) {
+
+		unsigned char c = 0;
+		float f = 0;
+int mZMax;
+int mYMax;
+int mXMax;
+Vec3f vec;
+		//Loop through data X changes first/fastest.
+		for (unsigned int iz = 0; iz < mZMax; iz++)
+			for (unsigned int iy = 0; iy < mYMax; iy++)
+				for (unsigned int ix = 0; ix < mXMax; ix++) {
+
+					//Initialise empty vector
+					my::Vec3f vec = my::Vec3f();
+
+					//Read x then y then z and store in vec (vector)
+					//Data needs converting to float from char and adjusted
+					myfile.read((char *)&c, 1);
+					f = (float)c;
+					vec.x = f/255-0.5;
+
+					myfile.read((char *)&c, 1);
+					f = (float)c;
+					vec.y = f/255-0.5;
+
+					myfile.read((char *)&c, 1);
+					f = (float)c;
+					vec.z = f/255-0.5;
+
+					//Store vector in datastructure
+					mData[iz][iy][ix] = vec;
+
+				}
+
+		//Close the file when finished
+		myfile.close();
+	}
+}
+
